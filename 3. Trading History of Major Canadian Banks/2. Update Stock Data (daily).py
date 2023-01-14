33761f43c0ea19b02c1516b9880226e2f67f7868
jaeykos <112841757+jaeykos@users.noreply.github.com>
2023-01-14 11:01:09 -0700

    Update 2. Update Stock Data (daily).py
    Prevent duplicate dates & Check individual sheet
    
    - Previous version sometimes appended tables with data of already existing dates. Updated sqldf statement to filter dates to be equal or greater than dayAfterLastDay. 
    - Previous version only checked dates of first sheet, assuming other sheets' dates are the same. Rearranged code to check dates for each sheet.

--- 3. Trading History of Major Canadian Banks/2. Update Stock Data (daily).py
+++ 3. Trading History of Major Canadian Banks/2. Update Stock Data (daily).py
@@ -13,29 +13,39 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("stock-367302-c665cb9043f9.json", scope)
 client = gspread.authorize(creds)
-
-#set date variables of last updated date and tomorrow
-
-wb = client.open("Canadian Bank Stock Price 3yrs") # access google sheet file with the name in quote
-sheet = wb.worksheet("TD") #access the first sheet "TD" in the file
-last_row = len(sheet.col_values(1)) # the last row in the sheet
-strLastDay = sheet.cell(last_row, 1).value # the date of last row
-LastDay = datetime.strptime(strLastDay, '%Y-%m-%d') #convert to datetime variable for date calculation
-DayAfterLastDay = LastDay + timedelta(days = 1) #calculate 1 day after last date in table
-strDayAfterLastDay = DayAfterLastDay.strftime('%Y-%m-%d') # convert to string (yfinance only takes string)
-strToday = (date.today()).strftime('%Y-%m-%d') # today's date in string
+wb = client.open("Canadian Bank Stock Price 3yrs")  # access google sheet file with the name in quote
 
 # set up lists for populating each Google sheet through loop
-bankList = ["TD", "RB", "Scotia", "CIBC", "BMO", "NB"]  # these are tab names in Google sheet
-tickerList = ["TD.TO", "RY.TO", "BNS.TO", "CM.TO", "BMO.TO", "NA.TO"]  # these are tickers of stocks
+bankList = ["TD", "RB", "Scotia", "CIBC", "BMO", "NB"]  # tab names in Google sheet
+tickerList = ["TD.TO", "RY.TO", "BNS.TO", "CM.TO", "BMO.TO", "NA.TO"]  # tickers of stocks for yfinance api
+
+today = date.today() # today's date
+strToday = today.strftime('%Y-%m-%d')  
 
 # update each sheet
-if strDayAfterLastDay != strToday: # if the dates are equal, that means table is already up-to-date
-    for bankName, ticker in zip(bankList, tickerList):
-        bankData = yf.download(ticker, start=strDayAfterLastDay, end=strToday)  # obtain stock information. yfinance returns pandas dataframe with Date as primary key
-        bankData = sqldf('SELECT date(Date) as Date, Close, Volume from bankData', globals())  # only keep Date, Close and Volume columns
+for bankName, ticker in zip(bankList, tickerList):
 
-        sheet = wb.worksheet(bankName)  # access tab in Google sheet that correspond to bank name
-        rowsToAppend = bankData.values.tolist()  # covert dataframe to list of rows as gspread api cannot handle dataframe
-        sheet.append_rows(rowsToAppend) #append table
-        sheet.delete_rows(2, 2 + len(rowsToAppend) - 1) # delete the first rows; delete same number of new rows added
+    sheet = wb.worksheet(bankName)
+    last_row = len(sheet.col_values(1))  # last row in sheet
+    lastDay = datetime.strptime(sheet.cell(last_row, 1).value, '%Y-%m-%d').date()  # date of last row
+    dayAfterLastDay = lastDay + timedelta(days=1)  # 1 day after LastDay
+    strDayAfterLastDay = dayAfterLastDay.strftime('%Y-%m-%d')  # convert to string for yfinance api
+
+    # only update sheet if strToday is later than strDayAfterLastDay
+    if today > dayAfterLastDay:
+
+        # get stock information. yfinance returns pandas dataframe with Date as primary key
+        bankData = yf.download(ticker, start=strDayAfterLastDay, end=strToday)
+
+        # extract Date, Close and Volume columns. also filter dates as yfinance sometimes return earlier dates
+        bankData = sqldf('SELECT date(Date) as Date, Close, Volume from bankData where Date >= \''
+                         + strDayAfterLastDay + '\' ', globals())
+
+        # covert dataframe to list of rows as gspread api cannot handle dataframe
+        rowsToAppend = bankData.values.tolist()
+
+        # append table
+        sheet.append_rows(rowsToAppend)
+
+        # delete the first rows; delete same number of new rows added
+        sheet.delete_rows(2, 2 + len(rowsToAppend) - 1)
